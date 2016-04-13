a336561aefa896c1bb46beb68191f781cf69277b
7777777707 <7777777707a@gmail.com>
2016-04-13 12:29:34 -0700

    lab 3 now

--- app/src/main/java/com/example/jdelcerr/exercise/MainActivity.java
+++ app/src/main/java/com/example/jdelcerr/exercise/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.jdelcerr.exercise;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,9 +9,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText firstNumber;
+    private EditText secondNumber;
+    private TextView addResult;
+    private Button btnAdd;
+
+    double num1,num2,sum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +34,21 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        firstNumber = (EditText)findViewById(R.id.editText);
+        secondNumber = (EditText)findViewById(R.id.editText2);
+        addResult = (TextView)findViewById(R.id.textView);
+        btnAdd = (Button)findViewById(R.id.button);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                num1 = Double.parseDouble(firstNumber.getText().toString());
+                num2 = Double.parseDouble(secondNumber.getText().toString());
+                sum = num1 + num2;
+                addResult.setText(Double.toString(sum));
             }
         });
     }
